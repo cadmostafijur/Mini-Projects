dc60fc58eb3b09e1bd0ed741f7f78d98196448d9
Md Mostafijur Rahman <104758273+hacker75msl@users.noreply.github.com>
2022-09-06 04:08:08 +0600

    Update fib.py

--- fib.py
+++ fib.py
@@ -5,3 +5,21 @@ def fib(n):
         return fib( n-1) + fib(n-2 )
 n=int(input())
 print(fib(n))
+
+
+
+
+=======================*************===========================
+# using memoization with the fibonacci sequence
+cache = { } # used to cache values to be used later
+def fib(n):
+    if n in cache:
+        return cache[ n ] # return value stored in dictionary
+    result = 0 # base case
+    if n <= 1:
+        result = n
+    else:
+        result = fib(n-1) + fib( n-2 )
+        cache[ n ] = result # save result into dictionary with n as the key
+    return result
+print( fib(50) ) # calculates almost instantly
