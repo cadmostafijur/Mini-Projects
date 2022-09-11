d2038716c860e7a58ad97db4c159b56474e5eef9
Md Mostafijur Rahman <104758273+hacker75msl@users.noreply.github.com>
2022-09-12 02:01:32 +0600

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,6 +2,6 @@ def factorial(n):# set your base case!
     if n <= 1:
         return 1
     else:
-        return factorial(n-1 ) * n
+        return factorial(n-1 ) * n #simple fac if n=4 then (4-1)*4=12 it returns 12
 n=int(input())
 print(factorial(n))
